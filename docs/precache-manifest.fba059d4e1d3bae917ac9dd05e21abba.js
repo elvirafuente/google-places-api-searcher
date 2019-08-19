0f1359061fed8ba94bdfbe07388638e340218c54
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2019-08-19 18:11:52 +0200

    try remote querys

---
+++ docs/precache-manifest.fba059d4e1d3bae917ac9dd05e21abba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3eff8a191392ad74a5dd2dba0de5b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eadc32dcd84ac970aa16",
+    "url": "./static/css/main.caf1dc23.chunk.css"
+  },
+  {
+    "revision": "a1e2eae126625e876b24",
+    "url": "./static/js/2.47f8e4a3.chunk.js"
+  },
+  {
+    "revision": "eadc32dcd84ac970aa16",
+    "url": "./static/js/main.662b2261.chunk.js"
+  },
+  {
+    "revision": "6779ae5e98331a99c755",
+    "url": "./static/js/runtime~main.bfb8b019.js"
+  }
+]);
